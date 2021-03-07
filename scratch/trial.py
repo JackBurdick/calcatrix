281a7ee978d75c1805b5232bed3e06a752a17b04
Jack Burdick <JackBurdick@users.noreply.github.com>
2021-03-06 17:56:05 -0800

    Include trial file

---
+++ scratch/trial.py
@@ -0,0 +1,28 @@
+from calcatrix import LinearDevice
+
+DIR_PIN = 27
+STEP_PIN = 17
+LOC_PIN = 23
+ENABLE_PIN = 22
+BOUND_A_PIN = 24
+BOUND_B_PIN = 25
+init_config = {
+    "stepper": {"dir": DIR_PIN, "step": STEP_PIN, "enable": ENABLE_PIN},
+    "location": {
+        "marker": {"pin": LOC_PIN},
+        "bound_a": {"pin": BOUND_A_PIN},
+        "bound_b": {"pin": BOUND_B_PIN},
+    },
+}
+ld = LinearDevice(init_config=init_config)
+
+# r_steps = 500
+# l_steps = 550
+
+ld.set_home()
+
+print(ld)
+
+# ld.move_direction(l_steps, True)
+# time.sleep(0.3)
+# ld.move_direction(r_steps, False)
