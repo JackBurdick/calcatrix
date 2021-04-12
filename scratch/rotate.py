32b97899553b9eed6b8e26100b763f054eaf469d
JackBurdick <jackbburdick@gmail.com>
2021-04-11 18:28:14 -0700

    Rotate to deg

--- scratch/rotate.py
+++ scratch/rotate.py
@@ -3,12 +3,17 @@ import RPi.GPIO as GPIO
 from calcatrix.devices.rotate import Rotator
 
 if __name__ == "__main__":
+
+    deg = 35
+
     GPIO.setmode(GPIO.BCM)
     cur_pins = [21, 20, 16, 12]
     m = Rotator(cur_pins)
     m.rpm = 5
     print(f"Pause in seconds: {m._T}")
-    m.move_to(90)
+    m.move_to(deg)
+    sleep(1)
+    m.move_to(360-deg)
     sleep(1)
     m.move_to(0)
     sleep(1)
