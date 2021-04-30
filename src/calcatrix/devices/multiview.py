eaeac2059b1c6ca81cc2ac65309c8f8b58523035
Jack Burdick <JackBurdick@users.noreply.github.com>
2021-04-29 21:29:03 -0700

    Build view locations

--- src/calcatrix/devices/multiview.py
+++ src/calcatrix/devices/multiview.py
@@ -30,11 +30,27 @@ class MultiView:
         self._angle_a = angle / 2
         self._angle_b = 360 - self._angle_a
 
+        self.view_locations = self.init_locations()
+
     def _set_angle_dist(self, dist, angle):
         return math.tan(angle * math.pi / 180) * dist
 
-    def init(self):
+    def init_locations(self):
         self.linear.set_home()
+
+        if not self.linear.positions:
+            raise ValueError("No positions present")
+
+        view_locs = {}
+        for ind, iloc in self.linear.positions.items():
+            # NOTE: unsure what the keys should be here
+            loc_d = {
+                "a": (iloc - self._dist, self._angle_a),
+                "0": (iloc, 0),
+                "b": (iloc + self._dist, self._angle_b),
+            }
+            view_locs[ind] = loc_d
+        return view_locs
 
     def __repr__(self):
         return str(self.__class__.__name__) + ": " + f"{self.__dict__}"
