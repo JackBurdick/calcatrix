e06e18a9bde3353c96d97a6c27591186c31f46cb
JackBurdick <jackbburdick@gmail.com>
2021-07-03 21:38:58 -0700

    Include basic image capture

--- scratch/trial.py
+++ scratch/trial.py
@@ -1,4 +1,7 @@
 from calcatrix.devices.multiview import MultiView  # pylint: disable=import-error
+from picamera import PiCamera
+from datetime import datetime
+import time
 
 DIR_PIN = 27
 STEP_PIN = 17
@@ -25,4 +28,37 @@ mv.initialize()
 
 print(mv)
 
-mv.follow_all_instructions()
+
+def take_photo(idict):
+    BASEPATH = "home/pi/dev/imgs/"
+    IMG_PATH_TEMPLATE = "{}__{}__{}.jpg"
+
+    # image index/bucket
+    try:
+        loc = idict["index"]
+    except KeyError:
+        loc = "X"
+
+    # view/rotation
+    try:
+        view = int(idict["rot_degree"])
+    except KeyError:
+        view = "X"
+
+    # e.g. '01_01_2021__17_13_18'
+    ts = datetime.now().strftime("%m_%d_%Y__%H_%M_%S")
+
+    filename = IMG_PATH_TEMPLATE.format(loc, view, ts)
+    filepath = f"/{BASEPATH}{filename}"
+
+    # capture image
+    with PiCamera() as camera:
+        camera.start_preview()
+        time.sleep(2)
+        try:
+            camera.capture(filepath)
+        finally:
+            camera.stop_preview()
+
+
+mv.follow_all_instructions(func=take_photo)
