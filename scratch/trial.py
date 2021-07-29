413a52401ba45cadb7e1c09e35c6ea24b16aca5c
Jack Burdick <JackBurdick@users.noreply.github.com>
2021-07-28 20:45:13 -0700

    photo function

--- scratch/trial.py
+++ scratch/trial.py
@@ -1,7 +1,10 @@
-from calcatrix.devices.multiview import MultiView  # pylint: disable=import-error
-from picamera import PiCamera
-from datetime import datetime
 import time
+from datetime import datetime
+
+from picamera import PiCamera
+
+from calcatrix.devices.multiview import MultiView  # pylint: disable=import-error
+from calcatrix.functions.photo import take_photo  # pylint: disable=import-error
 
 DIR_PIN = 27
 STEP_PIN = 17
@@ -34,37 +37,5 @@ mv = MultiView(init_config=init_config)
 mv.initialize()
 
 print(mv)
-
-
-def take_photo(idict):
-    BASEPATH = "home/pi/dev/imgs/"
-    IMG_PATH_TEMPLATE = "{}__{}__{}.jpg"
-
-    # image index/bucket
-    try:
-        loc = idict["index"]
-    except KeyError:
-        loc = "X"
-
-    # view/rotation
-    try:
-        view = int(idict["rot_degree"])
-    except KeyError:
-        view = "X"
-
-    # e.g. '01_01_2021__17_13_18'
-    ts = datetime.now().strftime("%m_%d_%Y__%H_%M_%S")
-    filename = IMG_PATH_TEMPLATE.format(loc, view, ts)
-    filepath = f"/{BASEPATH}{filename}"
-
-    # capture image
-    with PiCamera() as camera:
-        camera.start_preview()
-        time.sleep(2)
-        try:
-            camera.capture(filepath)
-        finally:
-            camera.stop_preview()
-
 
 mv.follow_all_instructions(func=take_photo)
